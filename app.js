115daa6c2b63bb04b374a8dbe906cee05d8af41d
humantrop <trope93@gmail.com>
2020-09-28 23:55:19 +0200

    My first Java Script project

---
+++ app.js
@@ -0,0 +1,25 @@
+const switchButton = document.querySelector(".dn-switch");
+const light = document.querySelector(".light");
+const dark = document.querySelector(".dark");
+const title = document.querySelector(".heading");
+const theBody = document.querySelector("body");
+const sunIcon = document.querySelector(".sun-above");
+const mooonIcon = document.querySelector(".moon-above");
+
+dark.addEventListener("click", function () {
+  title.classList.add("heading-dark");
+  title.innerHTML = "Good Night";
+  theBody.classList.add("body-dark");
+  dark.classList.add("light-to-dark");
+  mooonIcon.classList.add("moon-dark");
+  sunIcon.classList.add("sun-dark");
+});
+
+light.addEventListener("click", function () {
+  title.classList.remove("heading-dark");
+  title.innerHTML = "Good Day";
+  theBody.classList.remove("body-dark");
+  dark.classList.remove("light-to-dark");
+  mooonIcon.classList.remove("moon-dark");
+  sunIcon.classList.remove("sun-dark");
+});

ce6a323408fd758ab86717839b4f89f6c3702782
humantrop <trope93@gmail.com>
2020-09-30 09:37:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ light.addEventListener("click", function () {
   cursor.classList.remove("cursor-dark");
 });
 
-/// Zezanje
+/// Fun
 
 document.addEventListener("mousemove", function (e) {
   let x = e.clientX;
@@ -35,37 +35,3 @@ document.addEventListener("mousemove", function (e) {
   cursor.style.left = x + "px";
   cursor.style.top = y + "px";
 });
-
-function getDateTime() {
-  var now = new Date();
-  var year = now.getFullYear();
-  var month = now.getMonth() + 1;
-  var day = now.getDate();
-  var hour = now.getHours();
-  var minute = now.getMinutes();
-  var second = now.getSeconds();
-  if (month.toString().length == 1) {
-    month = "0" + month;
-  }
-  if (day.toString().length == 1) {
-    day = "0" + day;
-  }
-  if (hour.toString().length == 1) {
-    hour = "0" + hour;
-  }
-  if (minute.toString().length == 1) {
-    minute = "0" + minute;
-  }
-  if (second.toString().length == 1) {
-    second = "0" + second;
-  }
-  var dateTime =
-    year + "/" + month + "/" + day + " " + hour + ":" + minute + ":" + second;
-  return dateTime;
-}
-
-// example usage: realtime clock
-setInterval(function () {
-  currentTime = getDateTime();
-  document.getElementById("digital-clock").innerHTML = currentTime;
-}, 1000);
